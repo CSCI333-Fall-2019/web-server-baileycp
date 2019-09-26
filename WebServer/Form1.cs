294159034f9cd45b3c557f77f72745223e89cc12
bpeticolas <bailey.peticolas@principia.edu>
2019-09-25 22:57:30 -0500

    Finished project

--- WebServer/Form1.cs
+++ WebServer/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,25 @@ namespace WebServer
         public Form1()
         {
             InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            ThreadPool.QueueUserWorkItem(delay, this);
+        }
+
+        private void delay(object state)
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
         }
     }
 }
