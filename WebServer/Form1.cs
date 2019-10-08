e81d506b299508583a3394a1eef0cfd0e3289a4d
bpeticolas <bailey.peticolas@principia.edu>
2019-10-07 23:37:37 -0500

    Minor update

--- WebServer/Form1.cs
+++ WebServer/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ namespace WebServer
 {
     public partial class Form1 : Form
     {
+        TcpListener listener;
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -23,18 +28,63 @@ namespace WebServer
         {
             button1.Enabled = false;
             button2.Enabled = true;
-            ThreadPool.QueueUserWorkItem(delay, this);
-        }
-
-        private void delay(object state)
-        {
-            Thread.Sleep(5000);
+            ThreadPool.QueueUserWorkItem(listenerFunc, this); // keeps UI responsive
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             button1.Enabled = true;
             button2.Enabled = false;
+        }
+
+        private bool _isRunning;
+        static readonly object _isRunningLock = new object();
+        private int port = 1234;
+
+        public bool IsRunning
+        {
+            get
+            {
+                lock (_isRunningLock)
+                {
+                    return this._isRunning;
+                }
+            }
+            set
+            {
+                lock (_isRunningLock)
+                {
+                    this._isRunning = value;
+                }
+                button1.Enabled = !value;
+                button2.Enabled = value;
+            }
+        }
+
+        private void listenerFunc(object tempInfo)
+        {
+            Form1 main = (Form1)tempInfo;
+            main.listener = new TcpListener(IPAddress.Any, main.port);
+            main.listener.Start();
+
+            while (main.IsRunning)
+            {
+                try
+                {
+                    TcpClient client = main.listener.AcceptTcpClient();
+                    ThreadPool.QueueUserWorkItem(GetRequestedItem, client);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error received: " + ex.Message);
+                }
+            }
+        }
+
+        private void GetRequestedItem(object state)
+        {
+            TcpClient client = (TcpClient)state;
+
         }
     }
 }
